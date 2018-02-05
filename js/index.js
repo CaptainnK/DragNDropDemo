f96af64600a7b5133febbb72541309b9e67ef30b
Kyle <kdrewno@umich.edu>
2018-02-05 16:56:47 -0500

    original

---
+++ js/index.js
@@ -0,0 +1,48 @@
+'use strict'
+
+var app = angular.module('example', ['as.sortable']);
+
+app.controller('MainCtrl', function ($scope) {
+
+  $scope.items = [{
+    name: 'Black Holes',
+    subjectcode: 'ASTR',
+    coursecode: '123'
+  }, {
+    name: 'Galaxies',
+    subjectcode: 'ASTR',
+    coursecode: '423'
+  }, {
+    name: 'Supernovas',
+    subjectcode: 'ASTR',
+    coursecode: '167'
+  }, {
+    name: 'Aliens',
+    subjectcode: 'ASTR',
+    coursecode: '657'
+  }, {
+    name: 'Planets',
+    subjectcode: 'ASTR',
+    coursecode: '6907'
+  }, {
+    name: 'Stars',
+    subjectcode: 'ASTR',
+    coursecode: '7686'
+  }, {
+    name: 'Mars',
+    subjectcode: 'ASTR',
+    coursecode: '78467'
+  }, {
+    name: 'Sun',
+    subjectcode: 'ASTR',
+    coursecode: '657'
+  }];
+  
+  $scope.$watch('items', function () {
+    console.log(arguments);
+  });
+
+  $scope.sortableOptions = {
+    containment: '#sortable-container'
+  };
+});
